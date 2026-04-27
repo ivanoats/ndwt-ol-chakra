1acb2f6effd66eab8608353532403caf03fab3ee
Ivan Storck <ivanoats@gmail.com>
2026-04-27 16:07:30 -0700

    fix: localhost at home

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ['192.168.0.115'],
   output: 'export',
   images: { unoptimized: true },
   reactStrictMode: true,
